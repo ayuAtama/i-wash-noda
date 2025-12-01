c7a4dd80dbe5bdec9af8d44722757d0b4fccd7eb
Wahyu Pratama <ayuramatama@gmail.com>
2025-12-01 17:05:35 +0700

    chore: change the token colomn into unique and use bcrypt instead of sha256

--- apps/api/src/utils/tokenGenerator.ts
+++ apps/api/src/utils/tokenGenerator.ts
@@ -1,11 +1,25 @@
 import crypto from "crypto";
+import bcrypt from "bcrypt";
+import "dotenv/config";
 
 const generate6DigitCode = () => {
   return crypto.randomInt(0, 999999).toString().padStart(6, "0");
 };
 
+// const hashToken1 = (token: string) => {
+//   return crypto.createHash("sha256").update(token).digest("hex");
+// };
+
+// predetermined salt
+// const saltRounds: number = Number(process.env.BCRYPT_TOKEN);
+// const salt = bcrypt.genSaltSync(saltRounds);
+// const hashPassword = (password: string) => {
+//   return bcrypt.hashSync(password, salt);
+// };
+
 const hashToken = (token: string) => {
-  return crypto.createHash("sha256").update(token).digest("hex");
+  const saltRounds = Number(process.env.BCRYPT_TOKEN);
+  return bcrypt.hashSync(token, saltRounds); // bcrypt generates a new salt automatically
 };
 
 export { generate6DigitCode, hashToken };
